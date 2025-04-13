c40debbbc0045e90fe24b976fe62da3d9a4abfc3
frostyflame81 <jpyeates@gmail.com>
2025-04-13 19:26:13 +0100

    fixed bad syntax in replace statement

--- app.py
+++ app.py
@@ -705,7 +705,7 @@ async def test_store_basic_profile(neohub_name: str) -> None:
 
     try:
         # Use the neohubapi library's _send function directly
-        store_profile_command = str(store_profile_command).replace("True", "true")("'",'"')  # Convert the command to a JSON string
+        store_profile_command = str(store_profile_command).replace("True", "true").replace("'",'"')  # Convert the command to a JSON string
         response = await hub._send(store_profile_command)
         #response = await send_message2(hub._client, store_profile_command)
 
