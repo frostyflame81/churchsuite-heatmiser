f0b09f4b5691526cba1c33be30c5764b06d3c823
frostyflame81 <jpyeates@gmail.com>
2025-04-13 17:53:07 +0100

    reordered STORE_PROFILE command to better match api syntax

--- app.py
+++ app.py
@@ -693,8 +693,8 @@ async def test_store_basic_profile(neohub_name: str) -> None:
     # Construct the STORE_PROFILE command
     store_profile_command = {
         "STORE_PROFILE": {
-            "name": "Test",
             "info": schedule_data,
+            "name": "Test"
         }
     }
 
